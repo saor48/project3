752a9352ad0eacce950132bf47a1b6cca6e0ce0c
Saor <saor@ezspeak.eu>
2018-07-29 08:36:48 +0000

    initial message parsing added

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-A chat application to teach basic english writing for non-native speakers.
-Each word is checked for correct spelling against a file of basic english words.
+A chat application to teach basic English writing for non-native speakers.
+Each word is checked for correct spelling against a file of basic English words.
 Words found as correct will be printed normally
 Words not found will be printed in brackets.
 
+Possible Additional Function:
+A user selected option of a second parse of each phrase entered will check for correct structure.
+User will enter line number of phrase to select.
+
+Additional Function 2:
+User may pre-indicate type of phrase as; greeting, statement, question. 
+Such phrase will then be checked for correct structure
--- run.py
+++ run.py
@@ -1,27 +1,68 @@
 import os
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template, request
+import csv
 
 app = Flask(__name__)
-phrases = []
+phrases = []    
+users = []      
+chatters = 0    #current number of users online
+lines = 0       #enumerate each chat line
 
-def add_phrase(username, message):
-    phrases.append("{0}: {1}".format(username, message))
+def add_phrase(username, message, lines):
+    phrases.append("{2}> {0}: {1}".format(username, message, lines))
     
-def get_phrases():
-    return "<br>".join(phrases)
+def split_message(message):
+    words = message.split(' ')
+    return words
+
+def parse_phrases(phrase, words):
+    corrected = ""
+    with open("data/wordlist.txt", "r") as word_list:
+        reader = csv.reader(word_list, delimiter=',')
+        for row in reader:
+            wordlist = row
+        print("yes", wordlist)
+        for word in words:
+            if word in wordlist:
+                corrected += " " + word
+                print("in-",word)
+            else:
+                word = '[' + word + ']'
+                corrected += " " + word
+                print("out-",word)
+    print("corrected-", corrected)
+    return corrected
     
-@app.route('/')
+@app.route('/', methods=["GET","POST"])
 def index():
-    return "<h4><i>to send a message</i> : /your-name/message</h4>"
+    global chatters
+    if request.method == "POST":
+        chatters += 1
+        if chatters == 1:
+            #erase old users from file
+            with open("data/users.txt", "w") as user_list:
+                 user_list.close()
+        with open("data/users.txt", "a") as user_list:
+            user_list.writelines(request.form["username"] + "\n")
+        return redirect(request.form["username"]) 
+    return render_template("index.html")
  
 @app.route('/<username>') 
 def username(username):
     username = username.title()
-    return "<h4>Hi {0} </h4>".format(username) + get_phrases()
+    with open("data/users.txt", "r") as user_list:  
+        users = user_list.readlines()
+    return render_template("chat.html", 
+                username=username, chat_messages=phrases, users=users, chatters=chatters )
     
 @app.route('/<username>/<message>') 
 def message(username, message):
-    add_phrase(username, message)
+    global lines
+    lines += 1
+    words = split_message(message)
+    message = parse_phrases(message, words)
+    add_phrase(username, message, lines)
+    print("global words-", words)
     return redirect(username)
     
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
