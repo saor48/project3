fa28b8afb32969891ab4c80d09d19f8fee1cdfce
Saor <saor@ezspeak.eu>
2018-08-05 09:44:59 +0000

    fixed reload error of POST data

--- run.py
+++ run.py
@@ -1,18 +1,19 @@
 """to fix
 Heroku - not resetttinf users,txt
-Timeout- NOT WORKING PROPERLY
+Timeout- affecting chat input box =>
+partial reload by div=jquery?
 """
 """to add
 list of verbs, nouns
 1. search phrase for verb
-2, look for noun before and after if statement
+2, look for noun before and after for statements
 """
 """option
 multi-thread searching
 
 """
 import os
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 import csv
 
 app = Flask(__name__)
@@ -20,18 +21,23 @@ phrases = []
 users = []      
 chatters = 0    #current number of users online
 lines = 0       #enumerate each chat line
+previous = {}   #store previous message for each user
 sourcelist = "data/wordlist.txt"
-     # from https://github.com/first20hours/google-10000-english
-sourcelist2 = "data/google-10000-english.txt"  
+     # adapted from https://github.com/first20hours/google-10000-english
+sourcelist2 = "data/google-10k-english.txt"  
 
 def add_phrase(username, message, lines):
     phrases.append("{2}> {0}: {1}".format(username, message, lines))
-    
-def split_message(message):
-    words = message.split(' ')
-    return words
 
-def parse_phrases(phrase, words):
+def previousMessage(username):
+    key = str(username)
+    if key in previous.keys():
+        message = previous[key]
+    else:
+        message = "xxx no previous message xxx"
+    return message
+    
+def parse_phrases(words):
     corrected = ""
     """
     with open(sourcelist, "r") as word_list:
@@ -64,17 +70,22 @@ def index():
  
 @app.route('/<username>',  methods=["GET","POST"]) 
 def username(username):
-    username = username.title()
+    global lines, previous
+    name = username.title()
     with open("data/users.txt", "r") as user_list:  
         users = user_list.readlines()
     if request.method == "POST":
         message = request.form["message"]
-        global lines
-        lines += 1
-        words = split_message(message)
-        message = parse_phrases(message, words)
-        add_phrase(username, message, lines)
+        if str(message) != "":
+            if str(message) != previousMessage(username):     #dont reload POST data
+                previous[str(username)] = str(message)
+                lines += 1
+                words = message.split(' ')
+                message = parse_phrases(words)
+                add_phrase(username, message, lines)
+            
     return render_template("chat.html", 
-                username=username, chat_messages=phrases, users=users, chatters=chatters )
+                username=name, chat_messages=phrases, users=users, chatters=chatters )
+  
   
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
