2343f08074add0b0d8874c8d16f5dff4f294c8ac
Saor <saor@ezspeak.eu>
2018-08-16 16:38:11 +0000

    added leaderboard

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 A chat application to teach basic English writing for non-native speakers.
-Each word is checked for correct spelling against a file of basic English words.
-Words found as correct will be printed normally
-Words not found will be printed in brackets.
+Expected input is a single phrase.
+App is multi-player to be limited to about 8 users
+
+Features:
+1. Each word is checked for correct spelling against a file of basic English words.
+    Words found as correct will be printed normally
+    Words not found will be printed in brackets.
+2. A score is assigned as +1 for a word in the list, -1 if not in the list.
+    Cumulative score is shown on user's page
+3. A leaderboard, based on above score, is shown on each page
 
 Possible Additional Function:
 A user selected option of a second parse of each phrase entered will check for correct structure.
--- run.py
+++ run.py
@@ -1,15 +1,18 @@
 """to fix
-Heroku - not reseting users.txt
+Heroku - not reseting users.txt -----
+----->use sessions or----->reuse setInterval to check if user online
 """
 """to add
-list of verbs, nouns
+0.list of verbs, nouns
 1. search phrase for verbs
+2. set chatroom limit =8
+3. user still online check
 """
 """option
 multi-thread searching 
-asyncio from python3.4 current python2.7
+asyncio - use for checking 3 above
 """
-import os
+import os, asyncio
 from flask import Flask, redirect, render_template, request, url_for
 import csv, json
 import structure
@@ -24,7 +27,10 @@ jsondict = {}
 chatters = 0    #current number of users online
 line = 0       #enumerate each chat line
 previous = {}   #store previous message for each user
-score = {}      #hold current score for each user
+score = {}      #hold current score for all users
+leaderboard = [] #sorted list by top scorer
+user_score = 0  #this username
+print("userscore-", user_score)
 sourcelist = "data/wordlist.txt"
      # adapted from https://github.com/first20hours/google-10000-english
 sourcelist2 = "data/8K-english.txt"  
@@ -33,7 +39,6 @@ def add_phrase(username, corrected, line):
     global jsondict
     jphrase = {}
     message = corrected[0]
-    score = corrected[1]
     chatline = "{2}> {0}: {1}".format(username, message, line)
     phrases.append(chatline)
     jphrase['line'] = str(line)
@@ -58,7 +63,30 @@ def previous_message(username):
     else:
         message = "xxx no previous message xxx"
     return message
+
+def calc_score(corrected, username):
+    global score, leaderboard
+    my_score = corrected[1]
+    if str(username) in score:
+        score[str(username)] += my_score
+    else:
+        score[str(username)] = my_score
+    my_score = score[str(username)]
+    calc_leaderboard(username, my_score)
+    return my_score
     
+def calc_leaderboard(username, my_score):
+    global leaderboard
+    if str(username) not in leaderboard:
+        leaderboard.append(str(username))
+    pos = leaderboard.index(str(username))
+    for k, v in score.items():
+        if my_score > score[str(k)]:
+            kpos = leaderboard.index(str(k))
+            if pos > kpos:
+                leaderboard.remove(str(username))
+                leaderboard.insert(kpos,str(username))
+                
 def parse_phrases(words):
     good=0
     bad=0
@@ -100,7 +128,7 @@ def index():
  
 @app.route('/<username>',  methods=["GET","POST"]) 
 def username(username):
-    global line, previous, score
+    global line, previous, score, user_score
     name = username.title()
     with open("data/users.txt", "r") as user_list:  
         users = user_list.readlines()
@@ -113,24 +141,21 @@ def username(username):
                 words = message.split(' ')
                 corrected = parse_phrases(words)
                 add_phrase(username, corrected, line)
-                if str(username) in score:
-                    score[str(username)] += corrected[1]
-                else:
-                    score[str(username)] = corrected[1]
+                #make def score instead
+                user_score = calc_score(corrected, username)
     # ------ this is temp position---------
     if line > 0:
         structure.parse(structure.getline('1')) 
     #---------------------------   
-    print("score-", score)
-    user_score=0
-    if str(username) in score:
-        user_score = score[str(username)]
     return render_template("chat.html", 
                 username=name, chat_messages=phrases, users=users, 
-                chatters=chatters, score=user_score)
+                chatters=chatters, my_score=user_score, 
+                score=score, leaderboard=leaderboard)
 
 @app.route('/chat/messages', methods=["GET","POST"])  
 def messages(): 
-  return render_template("messages.html", chat_messages=phrases)
+    #also here start a user timeout check -asyncio?
+  return render_template("messages.html", chat_messages=phrases,
+                score=score, leaderboard=leaderboard)
   
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
